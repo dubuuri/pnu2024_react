9a00a128d6ccbca941509aa7c2de7b2515d58f7f
yuri <dubu1012@pusan.ac.kr>
2024-07-08 11:47:13 +0900

    React 기초

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Hello from './01/Hello';
+import './01/Hello.css'
+// 내가 만든 component 함수는 태그가 됨 - 사용자 정의 태크
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <Hello />
     </div>
   );
 }
